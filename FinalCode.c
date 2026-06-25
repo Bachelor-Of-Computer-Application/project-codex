d642acdde215812a84c8e2d63e74ce43a0526c33
ankit16-hub <ankitpoudel744@gmail.com>
2026-06-25 09:53:20 +0545

    codes

--- FinalCode.c
+++ FinalCode.c
@@ -66,3 +66,55 @@ void loadData() {
 
     fclose(fp);
 }
+void signUp() {
+    clearScreen();
+    if (userCount >= MAX) {
+        printf("User limit reached!\n");
+        pauseScreen();
+        clearScreen();
+        return;
+    }
+    struct User u;
+    printf("Enter username: ");
+    scanf("%s", u.username);
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].username, u.username) == 0) {
+            printf("\nUsername already exists!\n");
+            pauseScreen();
+            clearScreen();
+            return;
+        }
+    }
+    printf("Enter password: ");
+    scanf("%s", u.password);
+    u.balance = 0;
+    u.transCount = 0;
+    users[userCount] = u;
+    userCount++;
+    saveData();
+    printf("\nAccount created successfully!\n");
+    pauseScreen();
+    clearScreen();
+}
+int login() {
+    clearScreen();
+    char username[50];
+    char password[50];
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+    for (int i = 0; i < userCount; i++) {
+        if (strcmp(users[i].username, username) == 0 &&
+            strcmp(users[i].password, password) == 0) {
+            printf("\nLogin successful!\n");
+            pauseScreen();
+            clearScreen();
+            return i;
+        }
+    }
+    printf("\nInvalid username or password!\n");
+    pauseScreen();
+    clearScreen();
+    return -1;
+}
